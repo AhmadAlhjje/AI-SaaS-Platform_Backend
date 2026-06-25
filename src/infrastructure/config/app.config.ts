79cb6eb56e066bcff356aa52b9de9edfec1bafad
Ahmad Obada Alhije <ahmadobadaal7jje@gmail.com>
2026-06-25 03:31:33 +0300

    بناء ال shared

--- src/infrastructure/config/app.config.ts
+++ src/infrastructure/config/app.config.ts
@@ -10,9 +10,9 @@ export interface AppConfig {
 export default registerAs(
   'app',
   (): AppConfig => ({
-    port: Number(process.env.PORT ?? 3000),
-    databaseUrl: process.env.DATABASE_URL ?? '',
-    jwtSecret: process.env.JWT_SECRET ?? '',
-    jwtExpiresIn: process.env.JWT_EXPIRES_IN ?? '15m',
+    port: Number(process.env.PORT),
+    databaseUrl: process.env.DATABASE_URL!,
+    jwtSecret: process.env.JWT_SECRET!,
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN!,
   }),
 );
